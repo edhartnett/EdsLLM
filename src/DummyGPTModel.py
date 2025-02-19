aad21fd1ced733c512ba3bf819217ad056eec243
Edward Hartnett <edward.hartnett@noaa.gov>
2025-02-19 08:00:09 -0700

    More code

--- src/DummyGPTModel.py
+++ src/DummyGPTModel.py
@@ -10,8 +10,26 @@ class DummyGPTModel(nn.Module):
         self.pos_emb = nn.embedding(cfg["context_length"] cfg["emb_dim"])
         self.drop_emb = nn.Dropout(cfg["drop_rate"])  
 
-
-        self.model = torch.nn.GPT2Model.from_pretrained('gpt2')
-        self.tokenizer = torch.nn.GPT2Tokenizer.from_pretrained('gpt2')
-        self.linear = nn.Linear(768, 1) # 768 is the hidden size of GPT2 
-         
+        self.trf_blocks = nn.Sequential(
+            *[DummyTransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+        
+        self.final_norm = DummyLayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"])
+        
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))  # (seq_len, emb_dim)
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x  self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+    
+class DummyTransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super(DummyTransformerBlock, self).__init__()
+        
+    def forward(self, x):
+        return x
