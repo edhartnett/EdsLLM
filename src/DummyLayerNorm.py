046394aaf618d14f06622b16f42c87689f703421
Edward Hartnett <edward.hartnett@noaa.gov>
2025-02-19 14:14:50 -0700

    more

--- src/DummyLayerNorm.py
+++ src/DummyLayerNorm.py
@@ -2,9 +2,16 @@
 import torch
 import torch.nn as nn
 
-class DummyLayerNorm(nn.Module):
-    def __init__(self, normalized_shape, eps=1e-5):
-        super(DummyLayerNorm, self).__init__()
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super(LayerNorm, self).__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
 
     def forward(self, x):
-        return x
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return norm_x * self.scale + self.shift
+    
