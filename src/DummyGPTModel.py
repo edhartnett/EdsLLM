e3710f9dba85c46bacca49d97444e01215c1f7af
Edward Hartnett <edward.hartnett@noaa.gov>
2025-02-20 10:39:33 -0700

    added generate_text_simple

--- src/DummyGPTModel.py
+++ src/DummyGPTModel.py
@@ -32,6 +32,31 @@ class EdsGPTModel(nn.Module):
         x = self.final_norm(x)
         logits = self.out_head(x)
         return logits
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    # idx is (B, T) array of indices in the current context
+    for _ in range(max_new_tokens):
+
+        # Crop current context if it exceeds the supported context size
+        # E.g., if LLM supports only 5 tokens, and the context size is 10
+        # then only the last 5 tokens are used as context
+        idx_cond = idx[:, -context_size:]
+
+        # Get the predictions
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        # Focus only on the last time step
+        # (batch, n_token, vocab_size) becomes (batch, vocab_size)
+        logits = logits[:, -1, :]
+
+        # Get the idx of the vocab entry with the highest logits value
+        idx_next = torch.argmax(logits, dim=-1, keepdim=True)  # (batch, 1)
+
+        # Append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
+
+    return idx
     
 
 def main():
@@ -46,36 +71,66 @@ def main():
         "qkv_bias": False
     }
     
-    tokenizer = tiktoken.get_encoding("gpt2")
-    batch = []
-    txt1 = "Every effort moves you"
-    txt2 = "Every day holds a"
-    batch.append(torch.tensor(tokenizer.encode(txt1)))
-    batch.append(torch.tensor(tokenizer.encode(txt2)))
-    batch = torch.stack(batch, dim=0)
-    print(txt1)
-    print(batch)
+    # tokenizer = tiktoken.get_encoding("gpt2")
+    # batch = []
+    # txt1 = "Every effort moves you"
+    # txt2 = "Every day holds a"
+    # batch.append(torch.tensor(tokenizer.encode(txt1)))
+    # batch.append(torch.tensor(tokenizer.encode(txt2)))
+    # batch = torch.stack(batch, dim=0)
+    # print(txt1)
+    # print(batch)
 
-    ffn = FeedForward.FeedForward(GPT_CONFIG_124M)
-    x = torch.randn(2, 10, GPT_CONFIG_124M["emb_dim"])
-    y = ffn(x)
-    print(y.shape)
-    print(y)
+    # ffn = FeedForward.FeedForward(GPT_CONFIG_124M)
+    # x = torch.randn(2, 10, GPT_CONFIG_124M["emb_dim"])
+    # y = ffn(x)
+    # print(y.shape)
+    # print(y)
     
-    torch.manual_seed(123)
-    x = torch.randn(2, 4, GPT_CONFIG_124M["emb_dim"])
-    block = TransformerBlock.TransformerBlock(GPT_CONFIG_124M)
-    output = block(x)
-    print(output.shape)
-    print(output)
+    # torch.manual_seed(123)
+    # x = torch.randn(2, 4, GPT_CONFIG_124M["emb_dim"])
+    # block = TransformerBlock.TransformerBlock(GPT_CONFIG_124M)
+    # output = block(x)
+    # print(output.shape)
+    # print(output)
 
+    torch.manual_seed(123)
     model = EdsGPTModel(GPT_CONFIG_124M)
-    logits = model(batch)
-    print(logits.shape)
-    print(logits)
+    model.eval()  # disable dropout
+
+    start_context = "Hello, I am"
+
+    tokenizer = tiktoken.get_encoding("gpt2")
+    encoded = tokenizer.encode(start_context)
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+
+    print(f"\n{50*'='}\n{22*' '}IN\n{50*'='}")
+    print("\nInput text:", start_context)
+    print("Encoded input text:", encoded)
+    print("encoded_tensor.shape:", encoded_tensor.shape)
+
+    out = generate_text_simple(
+        model=model,
+        idx=encoded_tensor,
+        max_new_tokens=10,
+        context_size=GPT_CONFIG_124M["context_length"]
+    )
+    decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+
+    print(f"\n\n{50*'='}\n{22*' '}OUT\n{50*'='}")
+    print("\nOutput:", out)
+    print("Output length:", len(out[0]))
+    print("Output text:", decoded_text)
+
+    # model = EdsGPTModel(GPT_CONFIG_124M)
+    # logits = model(batch)
+    # print(logits.shape)
+    # print(logits)
 
     total_params = sum(p.numel() for p in model.parameters())
     print(f"Total params: {total_params}")
+    total_size_mb = total_params * 4 / 1024 / 1024
+    print(f"Total size: {total_size_mb:.2f} MB")
     
 if __name__=="__main__":
     main()
